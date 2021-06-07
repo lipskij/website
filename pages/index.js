9932311e723eadfeef9b6b7ddf849fb6285a0615
Emil Lipskij <emil.lipskij@gmail.com>
2021-06-07 19:58:45 +0300

    Change project img

--- pages/index.js
+++ pages/index.js
@@ -162,7 +162,7 @@ export default function Home() {
             <Link href='https://covid-19-graph-ltu.netlify.app/'>
               <a rel='noopener' aria-label='project' target='_blank'>
                 <picture>
-                  <source srcSet='covid.webp' />
+                  <source srcSet='covid1.webp' />
                   <img ref={ref3} src='covid.png' alt='project' />
                 </picture>
               </a>
